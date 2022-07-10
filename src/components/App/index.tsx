693a63c2697a98f757c3aad04e053db06fc618a3
Tzu Yin <tzyn.wang@gmail.com>
2022-07-10 11:08:00 +0800

    [feat] add padding for TagGroups

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,15 @@
 import React, { memo } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 import TagGroups from '@Components/Common/TagGroups';
 
 function App(): React.ReactElement {
   /* Main */
   return (
     <CssBaseline>
-      <TagGroups />
+      <Box p={2}>
+        <TagGroups />
+      </Box>
     </CssBaseline>
   );
 }
